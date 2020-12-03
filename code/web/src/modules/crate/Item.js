d121f1f04c1bec31ade377557adef53fa5a5ee24
Michael Walker <Michael.e.walker.87@gmail.com>
2020-12-03 16:28:53 -0700

    Adds the Annotation for the Item.js

--- code/web/src/modules/crate/Item.js
+++ code/web/src/modules/crate/Item.js
@@ -1,40 +1,46 @@
 // Imports
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { Link, withRouter } from 'react-router-dom'
+// This is for a single item 
+import React, { PureComponent } from 'react' //imports react and PureComponent
+import PropTypes from 'prop-types' //to vaildate typing for the props 
+import { connect } from 'react-redux' //connects the redux store 
+import { Link, withRouter } from 'react-router-dom' 
+/* link does not seem to be used right 
+now, withRouter lets you use the histoty object */
 
 // UI Imports
-import Card from '../../ui/card/Card'
+// Imports the style embedded HTML elemtents 
+import Card from '../../ui/card/Card' 
 import Button from '../../ui/button/Button'
 import H4 from '../../ui/typography/H4'
 import Icon from '../../ui/icon'
 import { white, grey2, black } from '../../ui/common/colors'
 
 // App Imports
-import { APP_URL } from '../../setup/config/env'
-import userRoutes from '../../setup/routes/user'
-import { messageShow, messageHide } from '../common/api/actions'
-import { create } from '../subscription/api/actions'
+import { APP_URL } from '../../setup/config/env' // The root path
+import userRoutes from '../../setup/routes/user' // Has the routing objects for the specific user path 
+import { messageShow, messageHide } from '../common/api/actions' //are action that show the pop up message confirming subscribe
+import { create } from '../subscription/api/actions' // return a distpatch callback funtion that post the crate using Axios 
 
 // Component
+//PureComponent are class based componets that can give a speed boost, minmize the rerender amout of a componet
 class Item extends PureComponent {
 
   constructor(props) {
     super(props)
-
+    //the super might be coming as deprected due to the PureComponent
     this.state = {
-      isLoading: false
+      isLoading: false // controlls the button disabled state 
     }
   }
 
+
   onClickSubscribe = (crateId) => {
     this.setState({
-      isLoading: true
+      isLoading: true //disabling the button via setting to true
     })
-
+    // invokes messageShow
     this.props.messageShow('Subscribing, please wait...')
-
+    // invokes the create action to post a crate to the API 
     this.props.create({ crateId })
       .then(response => {
         if (response.data.errors && response.data.errors.length > 0) {
@@ -46,17 +52,17 @@ class Item extends PureComponent {
         }
       })
       .catch(error => {
-        this.props.messageShow('There was some error subscribing to this crate. Please try again.')
-      })
+        this.props.messageShow('There was some error subscribing to this crate. Please try again.') 
+      })// provides error handling
       .then(() => {
         this.setState({
-          isLoading: false
-        })
+          isLoading: false 
+        }) //enables the button 
 
         window.setTimeout(() => {
           this.props.messageHide()
         }, 5000)
-      })
+      }) //hides the loading message after 5 seconds 
   }
 
   render() {
@@ -99,10 +105,13 @@ Item.propTypes = {
 }
 
 // Component State
+// itemState is used in connect that is the behavior of the redux, sets the state 
 function itemState(state) {
   return {
     user: state.user
   }
 }
 
+// mapStateToProps === itemState and mapDispatchToProps === { create, messageShow, messageHide } 
+// withRouter is for the history object 
 export default connect(itemState, { create, messageShow, messageHide })(withRouter(Item))
