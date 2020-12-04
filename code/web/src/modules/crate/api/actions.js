f738be83af6580fd3e943d3cb7022d7ddd7bd0ac
Bret Merritt <merrittbret9@gmail.com>
2020-12-03 17:19:08 -0700

    Add code annotations to crate actions.js

--- code/web/src/modules/crate/api/actions.js
+++ code/web/src/modules/crate/api/actions.js
@@ -1,11 +1,12 @@
 // Imports
-import axios from 'axios'
-import { query, mutation } from 'gql-query-builder'
+import axios from 'axios' // The library used for requests and responses
+import { query, mutation } from 'gql-query-builder' // Library from original author of this project that includes templates for GraphQL
 
 // App Imports
-import { routeApi } from '../../../setup/routes'
+import { routeApi } from '../../../setup/routes' // Relative path to send our requests
 
 // Actions Types
+// Defining and exporting variables for our switch conditions for our reducers
 export const CRATES_GET_LIST_REQUEST = 'CRATES/GET_LIST_REQUEST'
 export const CRATES_GET_LIST_RESPONSE = 'CRATES/GET_LIST_RESPONSE'
 export const CRATES_GET_LIST_FAILURE = 'CRATES/GET_LIST_FAILURE'
@@ -18,12 +19,13 @@ export const CRATES_GET_FAILURE = 'CRATES/GET_FAILURE'
 // Get list of crates
 export function getList(orderBy = 'DESC', isLoading = true) {
   return dispatch => {
+    // Sets state to loading until post resolves
     dispatch({
       type: CRATES_GET_LIST_REQUEST,
       error: null,
       isLoading
     })
-
+    // Gets our crates from the API
     return axios.post(routeApi, query({
       operation: 'crates',
       variables: { orderBy },
@@ -31,6 +33,7 @@ export function getList(orderBy = 'DESC', isLoading = true) {
     }))
       .then(response => {
         if (response.status === 200) {
+          // Set state to successful list response
           dispatch({
             type: CRATES_GET_LIST_RESPONSE,
             error: null,
@@ -42,6 +45,7 @@ export function getList(orderBy = 'DESC', isLoading = true) {
         }
       })
       .catch(error => {
+        // Set state to failed list response
         dispatch({
           type: CRATES_GET_LIST_FAILURE,
           error: 'Some error occurred. Please try again.',
@@ -54,17 +58,20 @@ export function getList(orderBy = 'DESC', isLoading = true) {
 // Get single crate
 export function get(slug, isLoading = true) {
   return dispatch => {
+    // Sets state to isLoading until post resolves
     dispatch({
       type: CRATES_GET_REQUEST,
       isLoading
     })
 
+    // Gets a crate from the API
     return axios.post(routeApi, query({
       operation: 'crate',
       variables: { slug },
       fields: ['id', 'name', 'slug', 'description', 'image', 'createdAt']
     }))
       .then(response => {
+        // Set state to successful crate response
         dispatch({
           type: CRATES_GET_RESPONSE,
           error: null,
@@ -73,6 +80,7 @@ export function get(slug, isLoading = true) {
         })
       })
       .catch(error => {
+        // Set state to failed crate response
         dispatch({
           type: CRATES_GET_FAILURE,
           error: 'Some error occurred. Please try again.',
@@ -85,6 +93,7 @@ export function get(slug, isLoading = true) {
 // Get single crate by Id
 export function getById(crateId) {
   return dispatch => {
+    // Gets a crate by Id
     return axios.post(routeApi, query({
       operation: 'crateById',
       variables: { crateId },
@@ -95,9 +104,10 @@ export function getById(crateId) {
 
 // Create or update crate
 export function createOrUpdate(crate) {
+  // If the crate exists update it
   if (crate.id > 0) {
     return update(crate)
-  } else {
+  } else { // If the crate doesn't exist create it
     delete crate.id
     return create(crate)
   }
@@ -106,8 +116,8 @@ export function createOrUpdate(crate) {
 // Create crate
 export function create(variables) {
   return dispatch => {
-    return axios.post(routeApi, mutation({
-      operation: 'crateCreate',
+    return axios.post(routeApi, mutation({ // mutation template for GraphQL by original author 
+      operation: 'crateCreate', // This will send the crateCreate operation to the back-end to create a new crate
       variables,
       fields: ['id']
     }))
@@ -118,7 +128,7 @@ export function create(variables) {
 export function update(crate) {
   return dispatch => {
     return axios.post(routeApi, mutation({
-      operation: 'crateUpdate',
+      operation: 'crateUpdate', // This will send the crateUpdate operation to the back-end to update a crate
       variables: crate,
       fields: ['id']
     }))
@@ -129,7 +139,7 @@ export function update(crate) {
 export function remove(variables) {
   return dispatch => {
     return axios.post(routeApi, mutation({
-      operation: 'crateRemove',
+      operation: 'crateRemove', // This will send the crateRemove operation to the back-end to remove a crate
       variables,
       fields: ['id']
     }))
