120aeae4108168e0785405464c55bee85b5ddb7a
Daniel Lessenden <dless27@gmail.com>
2020-12-03 21:30:27 -0600

    Add BE annotations

--- code/api/src/modules/crate/model.js
+++ code/api/src/modules/crate/model.js
@@ -1,4 +1,6 @@
 'use strict'
+// The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
+// With strict mode, you can not, for example, use undeclared variables.
 
 module.exports = function(sequelize, DataTypes) {
   let Crate = sequelize.define('crates', {
@@ -11,8 +13,11 @@ module.exports = function(sequelize, DataTypes) {
   })
 
   Crate.associate = function(models) {
+    // similar to rails' has_many and belongs_to, build relationships with other objects
     Crate.hasMany(models.Subscription)
+    // crate has many Subscriptions, one to many relationship
   }
 
   return Crate
+  // Does not have implicit return 
 }
