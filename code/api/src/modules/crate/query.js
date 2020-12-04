9176eca9a3a06323f4019b8858c4e2fe3a165bd8
Daniel Lessenden <dless27@gmail.com>
2020-12-03 21:37:57 -0600

    Add BE annotations

--- code/api/src/modules/crate/query.js
+++ code/api/src/modules/crate/query.js
@@ -6,12 +6,15 @@ import CrateType from './types'
 import { getAll, getById } from './resolvers'
 
 // Crates All
+// This acts as a GET request, pulls in all existing crates
 export const crates = {
   type: new GraphQLList(CrateType),
   args: {
     orderBy: { type: GraphQLString }
+    // this returns the results ordered by a field ie ASC/DESC
   },
   resolve: getAll
+  // gets crates resolver method from resolver.js
 }
 
 // Crate By ID
@@ -21,4 +24,6 @@ export const crateById = {
     crateId: { type: GraphQLInt }
   },
   resolve: getById
+  // takes argument of the crate ID
+  // returns a single crate that matches ID 
 }
