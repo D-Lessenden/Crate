415ba6d573febc36700b7c4bdc1ac49f7f257513
Bret Merritt <merrittbret9@gmail.com>
2020-12-06 15:44:52 -0700

    Add selectedItemsInitialState and selectedItems, reformat surveyItems indenting

--- code/web/src/modules/survey/api/state.js
+++ code/web/src/modules/survey/api/state.js
@@ -1,5 +1,11 @@
 // Actions Types
-import { SURVEY_GET_ITEMS_REQUEST, SURVEY_GET_ITEMS_RESPONSE, SURVEY_GET_ITEMS_FAILURE } from './actions'
+import { 
+  SURVEY_GET_ITEMS_REQUEST, 
+  SURVEY_GET_ITEMS_RESPONSE, 
+  SURVEY_GET_ITEMS_FAILURE, 
+  ITEM_SELECT,
+  ITEM_DESELECT
+} from './actions'
 
 const surveyItemsInitialState = {
   isLoading: false,
@@ -10,20 +16,42 @@ const surveyItemsInitialState = {
 export const surveyItems = (state = surveyItemsInitialState, action) => {
   switch (action.type){
     case SURVEY_GET_ITEMS_REQUEST: 
-    return {
-      ...state, isLoading: action.isLoading,
-      error: null,
-    }
-    case SSURVEY_GET_ITEMS_RESPONSE: 
-    return {
-      ...state, isLoading: false,
-      error: action.error,
-      surveyItems: action.surveyItems,
-    }
+      return {
+        ...state, isLoading: action.isLoading,
+        error: null,
+      }
+    case SURVEY_GET_ITEMS_RESPONSE: 
+      return {
+        ...state, isLoading: false,
+        error: action.error,
+        surveyItems: action.surveyItems,
+      }
     case SURVEY_GET_ITEMS_FAILURE: 
-    return {
-      ...state, isLoading: false,
-      error: action.error,
-    }
+      return {
+        ...state, isLoading: false,
+        error: action.error,
+      }
+  }
+}
+
+const selectedItemsInitialState = {
+  selectedItems: []
+}
+
+export const selectedItems = (state = selectedItemsInitialState, action) => {
+  switch (action.type) {
+    case ITEM_SELECT:
+      return  {
+        ...state, selectedItems: [...state.selectedItems, action.item]
+      }
+    case ITEM_DESELECT:
+      const copyOfItems = state.selectedItems.concat()
+      const indexToRemove = copyOfItems.findIndex(item => {
+        return action.item === item
+      })
+      copyOfItems.splice(indexToRemove, 1)
+      return {
+        ...state, selectedItems: copyOfItems
+      }
   }
 }
