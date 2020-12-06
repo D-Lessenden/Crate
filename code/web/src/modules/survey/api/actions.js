7ab39861610cf6809d49f715a34b87a38d4a1c11
Michael Walker <Michael.e.walker.87@gmail.com>
2020-12-06 11:53:12 -0700

    Add the geySurveyItems function

---
+++ code/web/src/modules/survey/api/actions.js
@@ -0,0 +1,52 @@
+// Imports
+import axios from 'axios'
+import { query, mutation } from 'gql-query-builder'
+
+// App Imports
+import { routeApi } from '../../../setup/routes'
+
+// Actions Types
+export const SURVEY_GET_ITEMS_REQUEST = 'Survey/GET_ITEMS_REQUEST'
+export const SURVEY_GET_ITEMS_RESPONSE = 'Survey/GET_ITEMS_RESPONSE'
+export const SURVEY_GET_ITEMS_FAILURE = 'Survey/GET_ITEMS_FAILURE'
+
+export const getSurveyItems = (clothingType) => {
+
+  return (dispatch) => {
+    dispatch(
+      {
+        type: SURVEY_GET_ITEMS_REQUEST,
+        error: null,
+        isLoading: true,
+      }
+    )
+    return axios.post(routeApi, query({
+      operation:"getSurveyItems",
+      variables: {
+        clothingType
+      },
+      fields:["image", "score"]
+    }))
+    .then(response => {
+      if (response.status === 200) {
+        dispatch(
+          {
+            type: SURVEY_GET_ITEMS_RESPONSE,
+            error: null,
+            isLoading: false,
+            surveyItems: response.data.data.surveyItems,
+          }
+        )
+      } else {
+        console.error(response)
+      }
+    })
+    .catch(error => {
+      dispatch({
+        type: SURVEY_GET_ITEMS_FAILURE,
+        error: "Something went wrong, try reloading the page",
+        isLoading: false,
+      })
+    })
+  }
+} 
