d7a3f14b2916012d76aa72259e6f3d50f8af0fe8
Bret Merritt <merrittbret9@gmail.com>
2020-12-07 14:48:21 -0700

    Add Mike's branch's paired annotations

--- code/web/src/setup/client/App.js
+++ code/web/src/setup/client/App.js
@@ -1,13 +1,14 @@
 // Imports
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React from 'react' // Imports react 
+import { Route, Switch } from 'react-router-dom' //imports the routing 
 
 // App Imports
-import { routes } from '../../setup/routes'
-import Layout from '../../modules/common/Layout'
-import NotFound from '../../modules/common/NotFound'
-import RoutePrivate from '../../modules/auth/RoutePrivate'
+import { routes } from '../../setup/routes' //is the paths to to the file links
+import Layout from '../../modules/common/Layout' // overall layout styling 
+import NotFound from '../../modules/common/NotFound' // error handler for 404 and the like 
+import RoutePrivate from '../../modules/auth/RoutePrivate'// path logic for user login 
 
+//The wrapper for the application 
 const App = () => (
   <Layout>
     <Switch>
