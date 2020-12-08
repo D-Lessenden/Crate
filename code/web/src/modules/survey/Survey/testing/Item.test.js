40fd15842fb491b01d4ffe4ce0611b3a501f957b
Michael Walker <Michael.e.walker.87@gmail.com>
2020-12-08 16:44:36 -0700

    Set up the basic imports and test for Item survey

--- code/web/src/modules/survey/Survey/testing/Item.test.js
+++ code/web/src/modules/survey/Survey/testing/Item.test.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { Router } from "react-router-dom";
+import { MemoryRouter, Router } from "react-router-dom";
 import { createMemoryHistory } from 'history';
 import userEvent from '@testing-library/user-event';
-import Survey from './Survey';
+import Item from '../Item';
+
+describe('Survey Page', () => {
+  let image, item;
+  beforeEach(() => {
+    item = {
+      image: "image.jpg",
+      score: 9,
+    }; 
+    render(
+      <MemoryRouter>
+        <Item item = { item }/>
+      </MemoryRouter>
+    )
+    image = screen.getByRole("img") 
+  })
+
+  it('should display the survey item', () => {
+    expect(image).toHaveAttribute("src", "image.jpg")
+  })
+  
+  it('should change border color on click', () => {
+    userEvent.click(image)
+    expect(image).toHaveStyle({borderColor: "#CE9FFC"})
+  })
+  
+})
