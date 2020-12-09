df24e346a1f2c6914cb4bfdd1faf2b85780aa3be
Hashim Gari <hashmaster3k@gmail.com>
2020-12-09 16:30:53 -0700

    Add seeder for formal styles

--- code/api/src/seeders/formal.js
+++ code/api/src/seeders/formal.js
@@ -5,78 +5,78 @@ module.exports = {
     return queryInterface.bulkInsert('surveys', [
       {
         type: 'bottom',
-        image: 'public/images/survey/formal/mens_pants_formal.jpg',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/mens_pants_formal.jpg',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'accessory',
-        image: 'public/images/survey/formal/mens_shoes_formal.jpg',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/mens_shoes_formal.jpg',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'top',
-        image: 'public/images/survey/formal/mens_top_formal.jpg',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/mens_top_formal.jpg',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'accessory',
-        image: 'public/images/survey/formal/mens_watch_formal.jpg',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/mens_watch_formal.jpg',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'full',
-        image: 'public/images/survey/formal/mens_suit_formal.jpg',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/mens_suit_formal.jpg',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'full',
-        image: 'public/images/survey/formal/womans_dress_formal.png',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/womans_dress_formal.png',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'accessory',
-        image: 'public/images/survey/formal/womans_hat_formal.jpg',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/womans_hat_formal.jpg',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'bottom',
-        image: 'public/images/survey/formal/womans_pants_formal.jpg',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/womans_pants_formal.jpg',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'accessory',
-        image: 'public/images/survey/formal/womans_shoes_formal.jpeg',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/womans_shoes_formal.jpeg',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'top',
-        image: 'public/images/survey/formal/womans_top_formal.jpg',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/womans_top_formal.jpg',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         type: 'accessory',
-        image: 'public/images/survey/formal/womans_watch_formal.png',
-        score: [5, 0],
+        image: 'code/api/public/images/survey/formal/womans_watch_formal.png',
+        score: [0, 5],
         createdAt: new Date(),
         updatedAt: new Date()
       }
