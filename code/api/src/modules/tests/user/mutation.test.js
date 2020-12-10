48b99abca74383240bc8d27f190f865c614e83f2
Logan Riffell <lkriffell@gmail.com>
2020-12-10 13:43:43 -0700

    Remove id as an argument from setStyle in favor of using auth with passing tests
    Add mock for current user (current user id will always be 1)
    
    Add a guard for if a current user is logged in in updateStyle

--- code/api/src/modules/tests/user/mutation.test.js
+++ code/api/src/modules/tests/user/mutation.test.js
@@ -11,13 +11,12 @@ describe('user mutations', () => {
     it('can set a users style to casual', async (done) => {
         const response = await request(server)
             .post('/graphql')
-            .send({ query: 'mutation { setStyle(id: 1 style:"casual") { style id } }' })
+            .send({ query: 'mutation { setStyle(style:"casual") { style } }' })
             .expect(200);
           expect(response.body).toMatchObject({
             data: {
               setStyle: {
-                style: "casual",
-                id: 1
+                style: "casual"
               }
             }
           })
@@ -27,13 +26,12 @@ describe('user mutations', () => {
     it('can set a users style to formal', async (done) => {
         const response = await request(server)
             .post('/graphql')
-            .send({ query: 'mutation { setStyle(id: 1 style:"formal") { style id } }' })
+            .send({ query: 'mutation { setStyle(style:"formal") { style } }' })
             .expect(200);
           expect(response.body).toMatchObject({
             data: {
               setStyle: {
-                style: "formal",
-                id: 1
+                style: "formal"
               }
             }
           })
@@ -43,7 +41,7 @@ describe('user mutations', () => {
     it('cannot set a users style with blank string', async (done) => {
         const response = await request(server)
             .post('/graphql')
-            .send({ query: 'mutation { setStyle(id: 1 style:"") { style id } }' })
+            .send({ query: 'mutation { setStyle(style:"") { style } }' })
             .expect(200);
           expect(response.body.errors.[0].message).toBe("You must enter a style to set a user's style")
         done();
@@ -52,7 +50,7 @@ describe('user mutations', () => {
     it('cannot set a users style with the wrong data type', async (done) => {
         const response = await request(server)
             .post('/graphql')
-            .send({ query: 'mutation { setStyle(id: 1 style:7) { style id } }' })
+            .send({ query: 'mutation { setStyle(style:7) { style } }' })
             .expect(400);
           expect(response.body.errors.[0].message).toBe("Expected type String, found 7.")
         done();
--- code/api/src/modules/tests/utils.js
+++ code/api/src/modules/tests/utils.js
@@ -5,7 +5,13 @@ import schema from '../../setup/schema';
 let server = express();
 server.use("/", graphqlHTTP({
     schema: schema,
-    graphiql: false
+    graphiql: false,
+    context: {
+      auth: {
+        user: { id: 1 },
+        isAuthenticated: true
+      }
+    }
 }));
 
 module.exports = server;
