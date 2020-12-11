00e049afcc960225771f97639e1b819c392564be
Hashim Gari <hashmaster3k@gmail.com>
2020-12-11 11:33:11 -0700

    update utils

--- code/api/src/modules/tests/utils.js
+++ code/api/src/modules/tests/utils.js
@@ -8,7 +8,10 @@ server.use("/", graphqlHTTP({
     graphiql: false,
     context: {
       auth: {
-        user: { id: 1 },
+        user: { 
+          id: 1,
+           role: 'ADMIN'
+        },
         isAuthenticated: true
       }
     }
